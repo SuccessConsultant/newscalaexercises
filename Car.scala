dbad10503b68d02e85872795c1bf34c4cd5ab945
Emmanuel Haastrup <emmanuel.haastrup@gmail.com>
2017-06-15 22:23:43 +0100

    heart city code

---
+++ Car.scala
@@ -0,0 +1,8 @@
+package Garage
+
+/**
+  * Created by Adedotun Haastrup on 15/06/2017.
+  */
+class Car extends Vehicle {
+
+}
---
+++ Garage.scala
@@ -0,0 +1,10 @@
+package Garage
+
+/**
+  * Created by Adedotun Haastrup on 15/06/2017.
+  */
+object Garage {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
